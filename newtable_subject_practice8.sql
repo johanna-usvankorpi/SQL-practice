c03f6ec0db8c577171affc89e578ffabab611d3c
johanna-usvankorpi <johanna.usvankorpi@alumni.helsinki.fi>
2019-05-25 11:48:51 +0300

    create new table

---
+++ newtable_subject_practice8.sql
@@ -0,0 +1,4 @@
+INSERT INTO research.subject (first_name, last_name, gender, height, weight)
+VALUES ('matti', 'meikalainen', 'M', '179', '80'), ('maija', 'meikalainen', 'F', '162', '58'), ('ville', 'virtanen', 'M', '181', '100'),
+('tuukka', 'tolonen', 'M', '183', '92'), ('helmi', 'lehtovaara', 'F', '155', '49'), ('osmo', 'miettinen', 'M', '188', '102'),
+('melissa', 'makela', 'F', '165', '68'), ('maria', 'laine', 'F', '179', '70'), ('olavi', 'heikkinen', 'M', '190', '90'), ('sofia', 'jarvinen', '175', '60');
